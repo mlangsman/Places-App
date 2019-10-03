c90ea666b9c2b0d74a2d81f20c4cf3ccb1b5db3a
Marc <marc@langsman.com>
2019-10-03 17:57:39 +0100

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Marc Langsman on 03/10/2019.
+//  Copyright © 2019 Marc Langsman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
